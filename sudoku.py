2988d63e0c100d4c7e15c0d19952d96bcad1313a
hugh <hughbromund44@gmail.com>
2019-12-17 19:45:41 -0500

    printing board

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,35 @@
 import numpy
-import tkinter
 
 print ("Hello World")
+
+board = [[0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],]
+
+
+
+if len(board) != 9:
+    print("Board is of the wrong dimensions")
+
+
+# Function to add lines to the Board
+def printBoard(board):
+    for i in range(0,9):
+        print()
+        if i % 3 == 0 and i != 0:
+            print("----------------")
+        for j in range(0,9):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end = '')
+            print(board[i][j], end = '')
+    print()
+        
+        
+
+printBoard(board)
