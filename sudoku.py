d7cfe6c9e16affccc2bd0b1868a96d8fe999b0b5
hugh <hughbromund44@gmail.com>
2019-12-17 20:34:47 -0500

    Board Checking is working

--- sudoku.py
+++ sudoku.py
@@ -1,16 +1,26 @@
-import numpy
+import numpy as np
 
 print ("Hello World")
 
-board = [[0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0],]
+board = np.array([[1,2,3,4,5,6,7,8,9],
+        [2,3,4,5,6,7,8,9,1],
+        [3,4,0,0,0,0,0,0,0],
+        [4,5,0,0,0,0,0,0,0],
+        [5,6,0,0,0,0,0,0,0],
+        [6,7,0,0,0,0,0,0,0],
+        [7,8,0,0,0,0,0,0,0],
+        [8,9,0,0,0,0,0,0,0],
+        [9,1,0,0,0,0,0,0,0],])
+
+correctBoard = np.array([[3,7,9,2,4,5,8,6,1],
+        [2,8,5,3,6,1,9,7,4],
+        [1,6,4,9,7,8,2,3,5],
+        [9,5,8,6,3,4,1,2,7],
+        [4,3,7,1,9,2,6,5,8],
+        [6,2,1,8,5,7,4,9,3],
+        [7,9,2,4,1,3,5,8,6],
+        [8,4,3,5,2,6,7,1,9],
+        [5,1,6,7,8,9,3,4,2]])
 
 
 
@@ -30,6 +40,68 @@ def printBoard(board):
             print(board[i][j], end = '')
     print()
         
-        
 
 printBoard(board)
+
+# Takes in a Sudoku Board and checks if it is solved
+# in a valid way
+def checkBoard(board):
+    # Check if the rows contain 1-9 without repetition
+    for row in board:
+        # print("Checking rows")
+        check = [0,0,0,0,0,0,0,0,0]
+        for value in row:
+            if value <= 0 or value > 9:
+                print("The board has invalid entries")
+                return False
+            # If the value has already appeared we return false
+            if check[value - 1] == 1:
+                print("A value was duplicated in the Row")
+                print(check)
+                return False
+            # If this is the first time finding the value we update the array
+            if check[value - 1] == 0:
+                check[value - 1] = 1
+
+    # Check if the columns contain 1-9 without repetition
+    transpose = board.T
+    for row in transpose:
+        # print("Checking Column")
+        check = [0,0,0,0,0,0,0,0,0]
+        for value in row:
+            if value <= 0 or value > 9:
+                print("The board has invalid entries")
+                return False
+            # If the value has already appeared we return false
+            if check[value - 1] == 1:
+                print("A value was duplicated in the Column")
+                print(check)
+                return False
+            # If this is the first time finding the value we update the array
+            if check[value - 1] == 0:
+                check[value - 1] = 1
+    # Check if the 3x3 squares contain 1-9 without repetition
+    for i in range(0,3):
+        for j in range(0,3):
+            starCol = 3 * i
+            startRow = 3 * j
+            check = [0,0,0,0,0,0,0,0,0]
+            for k in range(starCol, starCol + 3):
+                for l in range(startRow, startRow + 3):
+                    value = board[k][l]
+                    if value <= 0 or value > 9:
+                        print("The board has invalid entries")
+                        return False
+                    # If the value has already appeared we return false
+                    if check[value - 1] == 1:
+                        print("A value was duplicated in the 3x3 area")
+                        print(check)
+                        return False
+                    # If this is the first time finding the value we update the array
+                    if check[value - 1] == 0:
+                        check[value - 1] = 1 
+    return True
+
+print(checkBoard(board))
+
+print(checkBoard(correctBoard))
